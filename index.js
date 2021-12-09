3e5b975d41a9ba4ddcf4b5ac005245add8565787
Luciano Lanes <llaneslopes@gmail.com>
2021-12-09 10:29:59 -0300

    Adicionado middleware de erro ao index

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const usersRouter = require('./api/routers/userRouters');
+const errorMiddleware = require('./api/middlewares/errorMiddleware');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT;
@@ -6,6 +9,10 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+
+app.use('/user', usersRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
 
